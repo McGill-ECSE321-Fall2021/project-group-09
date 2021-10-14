999ac098219933c6a0053bbaeecc856cc749ddf2
Sneha Singh <sneha.singh@mail.mcgill.ca>
2021-10-14 11:41:06 -0400

    setting up models package

---
+++ LibManSys-Backend/src/main/java/ca/mcgill/ecse321/projectgroup09/models/User.java
@@ -0,0 +1,10 @@
+package ca.mcgill.ecse321.projectgroup09.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.sql.Date;
+import java.sql.Time;
+
+public class User {
+
+}
