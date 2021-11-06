3eafdcdb7f6d1220e50a3ace3d452b20f9d8568d
Sneha Singh <sneha.singh@mail.mcgill.ca>
2021-11-06 11:04:56 -0400

    Booking Service

---
+++ LibManSys-Backend/src/main/java/ca/mcgill/ecse321/projectgroup09/service/BookingService.java
@@ -0,0 +1,17 @@
+package ca.mcgill.ecse321.projectgroup09.service;
+
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import ca.mcgill.ecse321.projectgroup09.dao.MemberRepository;
+import ca.mcgill.ecse321.projectgroup09.models.LibraryItem;
+import ca.mcgill.ecse321.projectgroup09.models.Member;
+import ca.mcgill.ecse321.projectgroup09.dao.LibraryItemRepository;
+
+@Service
+public class BookingService { // service class for booking out the library for event 
+
+}
