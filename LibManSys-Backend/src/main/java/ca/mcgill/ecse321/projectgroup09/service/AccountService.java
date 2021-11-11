ac7f69e72d5964680dbf8df118fef45b2d3d5bdf
Christos Cunning <christoscunn@gmail.com>
2021-11-10 19:44:43 -0500

    remove unneeded code from AccountService

--- LibManSys-Backend/src/main/java/ca/mcgill/ecse321/projectgroup09/service/AccountService.java
+++ LibManSys-Backend/src/main/java/ca/mcgill/ecse321/projectgroup09/service/AccountService.java
@@ -2,7 +2,6 @@ package ca.mcgill.ecse321.projectgroup09.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import ca.mcgill.ecse321.projectgroup09.dao.AccountRepository;
 
@@ -14,26 +13,5 @@ public class AccountService {
 	
 	@Autowired
 	private AccountRepository accountRepo;
-	
-	private Long nextId;
-	
-	
-	/**
-	 * Constructor
-	 */
-	public AccountService() {
-		nextId = 1L;
-	}
-	
-	/**
-	 * 
-	 * @return
-	 */
-	@Transactional
-	public Long getNextAccountId() {
-		Long nextId = this.nextId;
-		this.nextId++;
-		return nextId;
-	}
 	
 }
