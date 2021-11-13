61abe11cd91aff3387e00ef59a3b9c0649ab5fe2
abearafat44 <abearafat44@github.com>
2021-11-12 23:51:19 -0500

    create headlibrarian dto and service class

--- LibManSys-Backend/src/main/java/ca/mcgill/ecse321/projectgroup09/dto/HeadLibrarianDto.java
+++ LibManSys-Backend/src/main/java/ca/mcgill/ecse321/projectgroup09/dto/HeadLibrarianDto.java
@@ -1,5 +1,30 @@
 package ca.mcgill.ecse321.projectgroup09.dto;
 
-public class HeadLibrarianDto {
+import ca.mcgill.ecse321.projectgroup09.models.Library;
 
+public class HeadLibrarianDto {
+	private Long managerIDNum;
+	private LibraryDto library;
+	
+	public HeadLibrarianDto(){}
+	
+	public HeadLibrarianDto(Long managerIDNum){
+		this.managerIDNum = managerIDNum;
+	}
+	
+	public Long getManagerIDNum() {
+		return this.managerIDNum;
+	}
+	
+	public void setManagerIDNum(Long  library) {
+		this.managerIDNum =  library;
+	}
+	
+	public LibraryDto getLibrary() {
+		return this.library;
+	}
+	
+	public void setLibrary(LibraryDto library) {
+		this.library = library;
+	}
 }
--- LibManSys-Backend/src/main/java/ca/mcgill/ecse321/projectgroup09/service/HeadLibrarianService.java
+++ LibManSys-Backend/src/main/java/ca/mcgill/ecse321/projectgroup09/service/HeadLibrarianService.java
@@ -1,8 +1,12 @@
 package ca.mcgill.ecse321.projectgroup09.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HeadLibrarianService {
+	
+	@Autowired
+	private Long managerIDNum;
 
 }
