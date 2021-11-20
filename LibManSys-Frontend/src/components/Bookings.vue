8c60cd86042d72a3b2f024ecd9fe619380163b20
Sneha Singh <sneha.singh@mail.mcgill.ca>
2021-11-20 16:48:32 -0500

    Frontend pages

---
+++ LibManSys-Frontend/src/components/Bookings.vue
@@ -0,0 +1,5 @@
+<!---Bookings can be viewed by library members 
+(online or not, just to see when the library has been booked)
+
+Librarians should have access to a "New Booking" button and within a booking they 
+should be able to "Update Booking" and "Delete Booking"--> 
---
+++ LibManSys-Frontend/src/components/LibraryManagementDashboard.vue
@@ -0,0 +1,12 @@
+<!-- Only accessible by Librarians and Head Librarians
+
+Librarians: 
+- Create new Library Item 
+
+Head Librarian:
+- View all librarians (and create/fire librarians)
+- Set library schedule 
+- Set library hours 
+- Update library phone number and email address
+
+-->
