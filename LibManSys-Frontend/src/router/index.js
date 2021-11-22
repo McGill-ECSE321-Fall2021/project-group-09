331df07a5668d72e0c61d3c550f91f4ddd050bf2
rajaabk <rajaa.bk0@gmail.com>
2021-11-22 14:34:09 -0500

    Router Updated

--- LibManSys-Frontend/src/router/index.js
+++ LibManSys-Frontend/src/router/index.js
@@ -1,6 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Bookings from '@/components/Bookings'
+import Checkout from '@/components/Checkout'
+import LibrarianDashboard from '@/components/LibrarianDashboard'
+import LibrarianLogin from '@/components/LibrarianLogin'
+import LibraryInfo from '@/components/LibraryInfo'
+import LibraryManagementDashboard from '@/components/LibraryManagementDashboard'
+import MemberLogin from '@/components/MemberLogin'
+import MemberLoans from '@/components/MemberLoans'
+import OnlineMemberDashboard from '@/components/OnlineMemberDashboard'
+import Return from '@/components/Return'
+import Register from '@/components/Register'
+import SearchLibItems from '@/components/SearchLibItems'
+
 
 Vue.use(Router)
 
@@ -8,8 +21,78 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
+      name: 'Home',
       component: Hello
+    },
+    {
+      path: '/Bookings',
+      name: 'Bookings',
+      component: Bookings
+    },
+    {
+      path: '/Checkout',
+      name: 'Checkout',
+      component: Checkout
+    }
+    ,
+    {
+      path: '/LibrarianDashboard',
+      name: 'LibrarianDashboard',
+      component: LibrarianDashboard
+    }
+    ,
+    {
+      path: '/LibrarianLogin',
+      name: 'LibrarianLogin',
+      component: LibrarianLogin
+    }
+     ,
+    {
+      path: '/LibraryInfo',
+      name: 'LibraryInfo',
+      component: LibraryInfo
+    }
+    ,
+    {
+      path: '/LibraryManagementDashboard',
+      name: 'LibraryManagementDashboard',
+      component: LibraryManagementDashboard
+    }
+    ,
+    {
+      path: '/MemberLoans',
+      name: 'MemberLoans',
+      component: MemberLoans
+    }
+    ,
+    {
+      path: '/MemberLogin',
+      name: 'MemberLogin',
+      component: MemberLogin
+    }
+    ,
+    {
+      path: '/OnlineMemberDashboard',
+      name: 'OnlineMemberDashboard',
+      component: OnlineMemberDashboard
+    }
+    ,
+    {
+      path: '/Register',
+      name: 'Register',
+      component: Register
+    }
+    ,
+    {
+      path: '/Return',
+      name: 'Return',
+      component: Return
+    }
+    ,
+    {
+      path: '/SearchLibItems',
+      name: 'SearchLibItems',
+      component: SearchLibItems
     }
   ]
 })
