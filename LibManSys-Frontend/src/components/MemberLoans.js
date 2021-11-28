29d466b50d7601f3cf826c623ea6b4d45fbc34bd
rajaabk <rajaa.bk0@gmail.com>
2021-11-28 16:14:00 -0500

    Backend for Loans

--- LibManSys-Frontend/src/components/MemberLoans.js
+++ LibManSys-Frontend/src/components/MemberLoans.js
@@ -1,38 +1,84 @@
-function LoanDto(borrowedDate, returnDate, lateFees, loanStatus, loanId,
-	libraryItem, member, librarian) {
+import axios from 'axios'
+var config = require('../../config')
 
+var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
+var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+
+var AXIOS = axios.create({
+  baseURL: backendUrl,
+  headers: { 'Access-Control-Allow-Origin': frontendUrl }
+})
+function LoanDto(name,borrowedDate,returnedDate,lateFees, loanStatus,libraryItem, member, librarian) {
+	this.name = name;
 	this.borrowedDate = borrowedDate;
-	this.returnDate = returnDate;
+	this.returnedDate = returnedDate;
 	this.lateFees = lateFees;
 	this.loanStatus = loanStatus;
-	this.loanId = loanId;
 	this.libraryItem = libraryItem;
 	this.member = member;
 	this.librarian = librarian;
-
 }
+import MemberLogin from "../components/MemberLogin";
+import SearchLibItems from "../components/SearchLibItems";
+import OnlineMemberDashboard from "../components/OnlineMemberDashboard";
 
+import Router from "../router/index";
+import Register from "../components/Register";
 export default {
+	name: 'loan',
 	data() {
-
 		return {
 			loans: [],
 			newLoan: '',
+			errorLoan:'',
 			response: []
 		}
 	},
+	created: function () {
+    // Initializing persons from backend
+    AXIOS.get('/loans/get-all')
+    .then(response => {
+      // JSON responses are automatically parsed.
+      this.loans = response.data;
+      console.log(response.data);
+    })
+    .catch(e => {
+      this.errorLoan = e
+    })
+    
+  }
+,
 	methods: {
-	createLoan: function(borrowedDate, returnDate, lateFees, loanStatus, loanId,
-	libraryItem, member, librarian){
-		var p = new LoanDto(borrowedDate, returnDate, lateFees, loanStatus, loanId,
-	libraryItem, member, librarian);
+	createLoan: function(name,borrowedDate,returnedDate,lateFees, loanStatus,libraryItem, member, librarian){
+		var p = new LoanDto(name,borrowedDate,returnedDate,lateFees, loanStatus,libraryItem, member, librarian);
 		this.loans.push(p);
 		this.newLoan = '';
+	},
+	goToSubmitPage: function() {
+            Router.push({
+                path: "/MemberLogin",
+                name: "MemberLogin"
+            })
+        },
+        goToRegisterPage: function() {
+            Router.push({
+                path: "/Register",
+                name: "Register"
+            })
+        },
+        goToSearchPage: function() {
+            Router.push({
+                path: "/SearchLibItems",
+                name: "SearchLibItems"
+            })
+        },
+        goToMemberPage: function() {
+            Router.push({
+                path: "/OnlineMemberDashboard",
+                name: "OnlineMemberDashboard"
+            })
+        }
 	}
-	}
-	,
-	created: function() {
-		const l1 = new LoanDto("2022 - 09 - 01", "2022 - 09 - 09", 0, Active, 6, 3, 667802, 29460);
-		this.loans = [l1];
-	}
+	
+	
 }
