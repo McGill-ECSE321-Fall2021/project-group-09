ff6c3d9f27b575ab3e54a9db11a3d7916b8b394c
Omar <71576529+omar-abdelaziz-git@users.noreply.github.com>
2021-11-29 16:23:45 -0500

    Update AccountRepository.java

--- LibManSys-Backend/src/main/java/ca/mcgill/ecse321/projectgroup09/dao/AccountRepository.java
+++ LibManSys-Backend/src/main/java/ca/mcgill/ecse321/projectgroup09/dao/AccountRepository.java
@@ -12,7 +12,7 @@ import ca.mcgill.ecse321.projectgroup09.models.Account;
  */
 public interface AccountRepository extends CrudRepository<Account, String> {
 	
-	Account findAccountByAccountId(Long accountId);
+	Account findAccountByAccountID(Long accountID);
 	Account findAccountByFullName(String fullName);
 	
 }
