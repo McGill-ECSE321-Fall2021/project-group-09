cf07f97a6defca29a712038019564bb438339674
Christos Cunning <christoscunn@gmail.com>
2021-11-30 14:52:50 -0500

    update frontend config

--- LibManSys-Frontend/src/main.js
+++ LibManSys-Frontend/src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import VueSweetalert2 from 'vue-sweetalert2';
+//import VueSweetalert2 from 'vue-sweetalert2';
 import BootstrapVue from "bootstrap-vue"
 import App from './App'
 import router from './router'
@@ -22,7 +22,7 @@ Vue.config.productionTip = false
 window.EventBus = new Vue();
 
 //use Sweetalert
-Vue.use(VueSweetalert2);
+//Vue.use(VueSweetalert2);
 
 /* eslint-disable no-new */
 new Vue({
