afe35581d7344d432e7ca00da71b99e8084c005e
abearafat44 <abearafat44@github.com>
2021-12-06 21:17:02 -0500

    add framing in app

--- LibManSys-android/app/src/main/java/com/example/libmansys/MainActivity.kt
+++ LibManSys-android/app/src/main/java/com/example/libmansys/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.libmansys
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.net.Uri
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val url = "https://libmansys-frontend-g09a.herokuapp.com/#/"
+        val i = Intent(Intent.ACTION_VIEW)
+        i.data = Uri.parse(url)
+        startActivity(i)
     }
 }
